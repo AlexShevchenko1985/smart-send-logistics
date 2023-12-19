fa6c0e5cae311aef5bf12e14a4dc3fb5518cbfca
AlexShevchenko1985 <alexdoc1985@gmail.com>
2023-12-19 16:24:32 +0100

    Update install-wp-tests.sh

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -140,7 +140,7 @@ recreate_db() {
 }
 
 create_db() {
-	mysqladmin create $DB_NAME --user="$DB_USER" --password=
+	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
 }
 
 install_db() {
